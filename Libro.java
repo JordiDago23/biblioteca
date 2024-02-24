054142da7a47b5873ebb5c6e52fa01964c33ea54
JordiDago23 <148897047+JordiDago23@users.noreply.github.com>
2024-02-24 19:25:15 +0100

    Add files via upload

---
+++ Libro.java
@@ -0,0 +1,45 @@
+package biblioteca;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Libro extends materialL{
+	
+	public static boolean reservado = false;
+	public static ArrayList<Libro> arrayLibros = new ArrayList <>();
+	public static Scanner scan = new Scanner(System.in);
+//Erencia de materialL y reservado	
+public Libro(int id, String titulo, String autor, String tematica){
+	super(id, titulo, autor, tematica);
+	this.reservado = reservado;
+}
+
+//Getters y Setters getters
+	public boolean getReservado(){
+		return reservado;
+	}
+	
+	public void getResevado(boolean reservado){
+		this.reservado = reservado;
+	}
+	
+	public ArrayList<Libro> getArrayLibros(){
+		return arrayLibros;
+	}
+	
+	public void setArrayLibros(ArrayList<Libro> arrayLibros){
+		this.arrayLibros = arrayLibros;
+	}
+
+//Metodos
+public void modificar_reserva(){
+	int id_reserva;
+	
+	System.out.println("Introduzca la Id de su Libro para Modificar Reserva");
+	id_reserva = scan.nextInt();
+	
+	
+	
+}
+
+}
