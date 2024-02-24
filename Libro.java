85ff17b9b8315e14920e30e06e429767e98b5c22
JordiDago23 <148897047+JordiDago23@users.noreply.github.com>
2024-02-24 22:12:25 +0100

    biblioteca

--- Libro.java
+++ Libro.java
@@ -31,15 +31,74 @@ public Libro(int id, String titulo, String autor, String tematica){
 		this.arrayLibros = arrayLibros;
 	}
 
-//Metodos
-public void modificar_reserva(){
-	int id_reserva;
+//Metodos:
 	
-	System.out.println("Introduzca la Id de su Libro para Modificar Reserva");
-	id_reserva = scan.nextInt();
-//hola mari carmen
+	public void añadirLibro(Libro l) {
+        arrayLibros.add(l);
+    }
+	
+	public void eliminarLibro(Libro l) {
+        arrayLibros.remove(l);
+    }
 	
 	
-}
+	
+	
+	public void buscarLibroPorTitulo(String titulo, boolean reserva) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTitulo().equals(titulo)){
+            	mostarar(reserva);
+                return;
+            }
+        }
+        System.out.println("El  libro "+getTitulo()+", No ha sido encontrado en la Biblioteca.");
+	}
+	
+	public void buscarLibroPorAutor(String autor, boolean reserva) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getAutor().equals(autor)){
+            	mostarar(reserva);
+                return;
+            }
+        }
+        System.out.println("Los Libros del Autor : "+getAutor()+", No ha sido encontrados en la Biblioteca.");
+	}
+	
+	public void buscarLibroPorTematica(String tematica, boolean reserva) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTematica().equals(tematica)){
+            	mostarar(reserva);
+                return;
+            }
+        }
+        System.out.println("Los Libros de la Temática : "+getTematica()+", No ha sido encontrados en la Biblioteca.");
+	}
+	
+	
+	
 
+	public void eliminar_reserva(boolean reserva){
+		
+		if(reserva){
+			reserva = false;
+			System.out.println("La reserva del libro "+getTitulo()+", ha sido anulada.");
+		}else{
+			System.out.println("El  libro "+getTitulo()+", No ha sido reservado de momento.");
+		}
+	}
+
+	public void reservar_libro(boolean reserva){
+		String decidir;
+		if(!reserva){
+		reserva = true;
+		System.out.println("El  libro "+getTitulo()+", ha sido reservado por usted.");
+		}else{
+		System.out.println("El  libro "+getTitulo()+", ya ha sido reservado por un Cliente.");
+		}
+	}
+	
+	public void mostarar(boolean reserva){
+		System.out.println("Libro {Titulo: "+getTitulo()+" | Autor : "+getAutor()+" | Temática : "+getTematica()+" | Reservado :"+(reserva ? "SI":"NO")+" }");
+	}
+	
 }
