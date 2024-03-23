6b1282f5f270a9bf8689a951cbb4134d3a826d00
JordiDago23 <148897047+JordiDago23@users.noreply.github.com>
2024-03-23 19:58:38 +0100

    Libro
    modificaciones finales

--- Libro.java
+++ Libro.java
@@ -5,100 +5,148 @@ import java.util.Scanner;
 
 public class Libro extends materialL{
 	
-	public static boolean reservado = false;
-	public static ArrayList<Libro> arrayLibros = new ArrayList <>();
-	public static Scanner scan = new Scanner(System.in);
-//Erencia de materialL y reservado	
-public Libro(int id, String titulo, String autor, String tematica){
-	super(id, titulo, autor, tematica);
-	this.reservado = reservado;
-}
+	public  boolean reservado;
+    
+	public  static ArrayList<Libro> arrayLibros = new ArrayList<>();
+	public  static Scanner scan = new Scanner(System.in);
 
-//Getters y Setters getters
-	public boolean getReservado(){
-		return reservado;
-	}
-	
-	public void getResevado(boolean reservado){
-		this.reservado = reservado;
-	}
-	
-	public ArrayList<Libro> getArrayLibros(){
-		return arrayLibros;
-	}
-	
-	public void setArrayLibros(ArrayList<Libro> arrayLibros){
-		this.arrayLibros = arrayLibros;
-	}
-
-//Metodos:
-	
-	public void añadirLibro(Libro l) {
-        arrayLibros.add(l);
+    // Constructor
+    public Libro(int id, String titulo, String autor, String tematica) {
+       super( id,  titulo,  autor,  tematica);
+        this.reservado = false; 
     }
-	
-	public void eliminarLibro(Libro l) {
-        arrayLibros.remove(l);
+
+
+    // Getters y setters  
+    public boolean getReservado() {
+        return reservado;
     }
-	
-	
-	
-	
-	public void buscarLibroPorTitulo(String titulo, boolean reserva) {
-        for (Libro libro : arrayLibros) {
-            if (libro.getTitulo().equals(titulo)){
-            	mostarar(reserva);
-                return;
-            }
-        }
-        System.out.println("El  libro "+getTitulo()+", No ha sido encontrado en la Biblioteca.");
-	}
-	
-	public void buscarLibroPorAutor(String autor, boolean reserva) {
-        for (Libro libro : arrayLibros) {
-            if (libro.getAutor().equals(autor)){
-            	mostarar(reserva);
-                return;
-            }
-        }
-        System.out.println("Los Libros del Autor : "+getAutor()+", No ha sido encontrados en la Biblioteca.");
-	}
-	
-	public void buscarLibroPorTematica(String tematica, boolean reserva) {
-        for (Libro libro : arrayLibros) {
-            if (libro.getTematica().equals(tematica)){
-            	mostarar(reserva);
-                return;
-            }
-        }
-        System.out.println("Los Libros de la Temática : "+getTematica()+", No ha sido encontrados en la Biblioteca.");
-	}
-	
-	
-	
 
-	public void eliminar_reserva(boolean reserva){
-		
-		if(reserva){
-			reserva = false;
-			System.out.println("La reserva del libro "+getTitulo()+", ha sido anulada.");
-		}else{
-			System.out.println("El  libro "+getTitulo()+", No ha sido reservado de momento.");
-		}
-	}
+    public void setReservado(boolean reservado) {
+        this.reservado = reservado;
+    }
 
-	public void reservar_libro(boolean reserva){
-		String decidir;
-		if(!reserva){
-		reserva = true;
-		System.out.println("El  libro "+getTitulo()+", ha sido reservado por usted.");
-		}else{
-		System.out.println("El  libro "+getTitulo()+", ya ha sido reservado por un Cliente.");
-		}
-	}
-	
-	public void mostarar(boolean reserva){
-		System.out.println("Libro {Titulo: "+getTitulo()+" | Autor : "+getAutor()+" | Temática : "+getTematica()+" | Reservado :"+(reserva ? "SI":"NO")+" }");
-	}
-	
+    public static ArrayList<Libro> getArrayLibros() {
+        return arrayLibros;
+    }
+
+    public static void setArrayLibros(ArrayList<Libro> arrayLibros) {
+        Libro.arrayLibros = arrayLibros;
+    }
+
+    // AÑADIR LIBROS
+    public static void añadirLibro(Libro libro) {
+        arrayLibros.add(libro);
+    }
+    // ELIMINAR LIBROS
+    public static void eliminarLibro(String titulo) {
+        for(Libro libro : arrayLibros){
+        	if(libro.getTitulo().equals(titulo)){
+        		arrayLibros.remove(libro);
+        		System.out.println("El Libro : "+titulo+", ha sido eliminado.");
+        		return;
+        	}       	
+        }
+    	System.out.println("El Libro con el Titulo : "+titulo+", ha sido encontrada en la biblioteca.");
+    	
+    }
+    // MODIFICAR UNA LIBROS
+    public static void modificarLibro(String titulo, int nuevoId, String nuevoTitulo, String nuevoAutor, String nuevaTematica, boolean nuevoReservado) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTitulo().equals(titulo)) {
+                libro.setId(nuevoId);
+                libro.setTitulo(nuevoTitulo);
+                libro.setAutor(nuevoAutor);
+                libro.setTematica(nuevaTematica);
+                libro.setReservado(nuevoReservado);
+                System.out.println("El Libro : " + titulo + ", ha sido modificado exitosamente.");
+                return;
+            }
+        }
+        System.out.println("El Libro : " + titulo + ", no ha sido encontrado en la biblioteca.");
+    }
+
+    // BUSCAR LIBROS POR (TITULO/AUTOR/TEMATICA)
+    public static void buscarLibroPorTitulo(String tituloBuscar) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTitulo().equalsIgnoreCase(tituloBuscar)) {
+            	mostrarLibro(libro);
+                return;
+            }
+        }
+        System.out.println("El Libro : " + tituloBuscar + ", no ha sido encontrado en la biblioteca.");
+    }
+
+    public static void buscarLibroPorAutor (String autorBuscar) {
+        
+    	boolean encontrar = false;
+    	
+    	for (Libro libro : arrayLibros) {
+            if (libro.getAutor().equalsIgnoreCase(autorBuscar)) {
+            	mostrarLibro(libro);
+               System.out.println();
+               encontrar = true;
+            }
+        }
+    	if(!encontrar){
+        System.out.println("El Libro : " + autorBuscar + ", no ha sido encontrado en la biblioteca.");
+    	}
+    }
+    
+    
+    public static void buscarLibroPorTemática(String tematicaBuscar) {
+    	
+    	boolean encontrar = false;
+    	
+        for (Libro libro : arrayLibros) {
+            if (libro.getTematica().equalsIgnoreCase(tematicaBuscar)) {
+            	mostrarLibro(libro);
+            	System.out.println();
+            	encontrar = true;
+            }
+        }
+        if(!encontrar){
+        System.out.println("El Libro : " + tematicaBuscar + ", no ha sido encontrado en la biblioteca.");
+        }
+    }
+    
+    public static void eliminarReserva(String titulo) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTitulo().equals(titulo)) {
+                if (libro.getReservado()) {
+                    libro.setReservado(false);
+                    System.out.println("La reserva del Libro : "+titulo+", ha sido anulada.");
+                } else {
+                    System.out.println("El Libro : "+titulo+", no ha sido reservado.");
+                }
+                return;
+            }
+        }
+        System.out.println("El Libro : "+titulo+", no se encuentra en la biblioteca.");
+    }
+
+    public static void reservarLibro(String titulo) {
+        for (Libro libro : arrayLibros) {
+            if (libro.getTitulo().equals(titulo)) {
+                if (!libro.getReservado()) {
+                    libro.setReservado(true);
+                    System.out.println("El Libro : "+titulo+", ha sido reservado por usted.");
+                } else {
+                    System.out.println("El Libro : "+titulo+", ya ha sido reservado por un cliente.");
+                }
+                return;
+            }
+        }
+        System.out.println("El Libro : "+titulo+", no se encuentra en la biblioteca.");
+    }
+
+
+	public static void mostrarLibro(Libro libro) {
+        System.out.println("Información del libro:");
+        System.out.println("Título: " + libro.getTitulo());
+        System.out.println("Autor: " + libro.getAutor());
+        System.out.println("Tematica: " + libro.getTematica());
+        System.out.println("Reservado: " + (libro.getReservado() ? "Sí" : "No"));
+    }
+
 }
--- materialL.java
+++ materialL.java
@@ -1,11 +1,13 @@
 package biblioteca;
 
 public class materialL {
-//id, titulo, autor y temática 
-    private int id;
-    private String titulo;
-    private String autor;
-    private String tematica;
+	
+	//id, titulo, autor y temática
+	
+	protected int id;
+    protected String titulo;
+    protected String autor;
+    protected String tematica;
 
     public materialL(int id, String titulo, String autor, String tematica) {
         this.id = id;
@@ -13,7 +15,9 @@ public class materialL {
         this.autor = autor;
         this.tematica = tematica;
     }
+    
     // getters y setters
+    
     public int getId() {
         return id;
     }
@@ -45,7 +49,9 @@ public class materialL {
     public void setTematica(String tematica) {
         this.tematica = tematica;
     }
-
+    
+    //Metodos
+    
     public String cadena() {
         return "ID: " + id + ", Título: " + titulo + ", Autor: " + autor + ", Temática: " + tematica;
     }
